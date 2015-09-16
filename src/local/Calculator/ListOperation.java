e13edaf0c63348af9cae90d1edb87134929ddbe8
Calvin Burkhead <calvin.nivlac@gmail.com>
2015-09-16 00:57:40 -0700

    Added List operation interface as well as mean, median, and mode classes.

---
+++ src/local/Calculator/ListOperation.java
@@ -0,0 +1,8 @@
+package local.Calculator;
+import java.util.List;
+
+public interface ListOperation {
+	void setValues(List<Double> vales);
+	String getDescription(String desctiption);
+	double getResult();
+}
---
+++ src/local/Calculator/Mean.java
@@ -0,0 +1,25 @@
+package local.Calculator;
+
+import java.util.List;
+
+public class Mean implements ListOperation {
+
+	@Override
+	public void setValues(List<Double> vales) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getDescription(String desctiption) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getResult() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
